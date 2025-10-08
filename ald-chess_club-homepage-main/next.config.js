6ec87c2ae3dfa305f03f6c348d0a57ecf874efa2
Alv-P <alvinpaulp08@gmail.com>
2025-10-08 08:59:27 -0400

    Add files via upload

---
+++ ald-chess_club-homepage-main/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+const repoName = 'ald-chess_club-homepage'; // Change if your repo name is different
+
+const nextConfig = {
+  output: 'export',
+  basePath: isGithubPages ? `/${repoName}` : '',
+  assetPrefix: isGithubPages ? `/${repoName}/` : '',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
